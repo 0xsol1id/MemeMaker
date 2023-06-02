ed838a1ac02f83e526a08cac3002940923ed7b4a
0xsol1id <0xsol1d@soljunks.io>
2023-06-02 20:57:25 +0200

    main

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,4 +2,3 @@ export { DiscordLogo } from "./DiscordLogo";
 export { TwitterLogo } from "./TwitterLogo";
 export { MagicEdenLogo } from "./MagicEdenLogo";
 export { GithubLogo } from "./GithubLogo";
-export { ConnectWallet }from "./ConnectWallet";
