cd86d5a67f3b4be574cc9d63ff6ecdd74ce24779
0xsol1id <0xsol1d@soljunks.io>
2023-06-02 20:55:58 +0200

    main

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,6 @@
 import type { AppProps } from "next/app";
 import React, { useEffect, useMemo, useState } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import {
-  WalletModalProvider,
-} from '@solana/wallet-adapter-react-ui';
 
-require('@solana/wallet-adapter-react-ui/styles.css');
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import "../styles/App.css";
@@ -22,36 +16,10 @@ import "../styles/App.css";
 const endpoint = "https://compatible-smart-general.solana-mainnet.discover.quiknode.pro/9b4affb03539b7a422f5c636723e162c7a1b3afe/"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const network = WalletAdapterNetwork.Mainnet;
-  const wallets = useMemo(
-    () => [
-      /**
-       * Wallets that implement either of these standards will be available automatically.
-       *
-       *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
-       *     (https://github.com/solana-mobile/mobile-wallet-adapter)
-       *   - Solana Wallet Standard
-       *     (https://github.com/solana-labs/wallet-standard)
-       *
-       * If you wish to support a wallet that supports neither of those standards,
-       * instantiate its legacy wallet adapter here. Common legacy adapters can be found
-       * in the npm package `@solana/wallet-adapter-wallets`.
-       */
-    ],
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [network]
-  );
-    
+
   return (
     <div>
-      <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider wallets={wallets} autoConnect>
-          <WalletModalProvider>
-            {/*<SelectNetwork />*/}
-            <Component {...pageProps} />
-          </WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
+      <Component {...pageProps} />
     </div>
   );
 }
